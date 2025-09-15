35291e7dd6b8398cfb6aa5fe7d00c3b72d610a04
Oshadha Thambavita <osthambe20@gmail.com>
2025-09-15 14:10:41 +0530

    removed api key from the config stripe config file

--- src/main/java/edu/lk/ijse/gdse/aad/aadBackendFinal/config/StripeConfiguration.java
+++ src/main/java/edu/lk/ijse/gdse/aad/aadBackendFinal/config/StripeConfiguration.java
@@ -8,15 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StripeConfiguration {
 
-/*
     @Value("${stripe.secret}")
     private String stripeSecret;
 
-    */
 
     @PostConstruct
     public void initStripe() {
-        com.stripe.Stripe.apiKey = "sk_test_51S6y0y2M5zdyQQeDbIEGyjDvYQ76oLbxIjZFi91VmigIsk65RFaUAgChAzkQil0bBSMKsKYrvng8JKYKMid7fkxR005eXyQChX";
+        com.stripe.Stripe.apiKey = stripeSecret;
     }
 
 }
